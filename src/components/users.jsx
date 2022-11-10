b9fc44acfacb6e4b16b8cef87b9acb25b68b8d6c
Andrey Derunov <derunov.andreyy@gmail.com>
2022-11-10 08:47:56 +0400

    returned to factory settings

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -3,79 +3,8 @@ import api from "../api";
 
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll);
-  const badgeColor = users.length ? "bg-primary" : "bg-danger"; //цвет Badge
-  let badgeClasses = "badge m-2 "; //классы Badge
-  const formatText = () => {
-    if (users.length % 10 < 2 || users.length % 10 > 4 || users.length === 12) {
-      return `${users.length} человек тусанет с тобой сегодня`;
-    } else {
-      return `${users.length} человека тусанут с тобой сегодня`;
-    }
-  };
-  const badgeText = users.length ? formatText() : "Никто с тобой не тусанет"; //формируем текст для Badge в начале страницы
 
-  const renderBadge = () => {
-    badgeClasses += badgeColor;
-    return <span className={badgeClasses}>{badgeText}</span>;
-  };
-  const handleDeleteUser = (userId) => {
-    setUsers((prevState) => prevState.filter((user) => user._id !== userId));
-  };
-  const renderDeleteButton = (id) => {
-    return (
-      <button
-        id={id}
-        className="btn btn-danger"
-        onClick={(e) => handleDeleteUser(e.target.id)}
-      >
-        delete
-      </button>
-    );
-  };
-  const renderUser = (user) => {
-    return (
-      <tr key={user._id}>
-        <td>{user.name}</td>
-        <td>{user.profession.name}</td>
-        <td>
-          {user.qualities.map((quality) => {
-            let classes = "badge m-1 bg-";
-            classes += quality.color;
-
-            return (
-              <span key={quality.name} className={classes}>
-                {quality.name}
-              </span>
-            );
-          })}
-        </td>
-        <td>{user.completedMeetings}</td>
-        <td>{user.rate + " /5"}</td>
-        <td>{renderDeleteButton(user._id)}</td>
-      </tr>
-    );
-  };
-
-  return (
-    <>
-      <h1>{renderBadge()}</h1>
-      {Boolean(users.length) && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>{users.map((user) => renderUser(user))}</tbody>
-        </table>
-      )}
-    </>
-  );
+  return <></>;
 };
 
 export default Users;
