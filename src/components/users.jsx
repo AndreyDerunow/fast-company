673b61142041978e14aaed67bb3535a70b713bb3
Andrey Derunov <derunov.andreyy@gmail.com>
2022-11-20 05:58:34 +0400

    a few fixes

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-
+import SearchStatus from "./searchStatus";
 import User from "./user";
 
 const Users = ({ users, ...rest }) => {
   return (
     <>
-      {users.map((user) => (
-        <User key={user._id} {...user} {...rest} />
-      ))}
+      <SearchStatus number={users.length} />
+      {!!users.length && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col">Избранное</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <User key={user._id} {...user} {...rest} />
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
